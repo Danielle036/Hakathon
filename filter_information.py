51807365006b20982a3bb937e61e2195d6bc5768
Dana - Ezra <zinaezra@gmail.com>
2025-08-27 22:55:47 +0300

    get info

---
+++ filter_information.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import constants
+def get_information(food_service_dict):
+    food_service_panda=pd.DataFrame(food_service_dict)
+    # להכניס את זה בכל פעם לפייל ששומר גם את מה שהיה לפני זה
+    return food_service_panda
+food_service_panda = get_information(constants.food_service_dict)
+print(food_service_panda)
