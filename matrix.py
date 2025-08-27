57d42c9f512a31d32e88b4796f70a7590afb0143
Dana - Ezra <zinaezra@gmail.com>
2025-08-27 22:55:47 +0300

    get info

--- matrix.py
+++ matrix.py
@@ -3,17 +3,26 @@ from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 def create_matrix():
     matrix = []
-    for i in range(SCREEN_HEIGHT)//PIXEL_PER_BLOCK:
+    for i in range(SCREEN_HEIGHT//PIXEL_PER_BLOCK):
         list1 = []
-        for j in range(SCREEN_WIDTH)//PIXEL_PER_BLOCK:
+        for j in range(SCREEN_WIDTH//PIXEL_PER_BLOCK):
             matrix.append(list1)
     return matrix
 
 matrix = create_matrix()
-def screen_to_matrix(matrix1):
+def matrix_to_screen(matrix1,screen):
     dict_values = {}
-    for i in range(len(matrix1)):
-        for j in range(matrix1[i]):
+    for i in range(SCREEN_HEIGHT):
+        for j in range(SCREEN_WIDTH):
+            list1 = []
+            y = i
+            x = j
+            for v in range(PIXEL_PER_BLOCK):
+                    list1.append((x,y))
+                    x += 1
+            dict_values[(i//10,j//10)] = list1
+
+    return dict_values
 
 
 
