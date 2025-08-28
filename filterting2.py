b248cd4a36320f821a11c4fa85a7c402f9ac90c0
OrBittonHamalka <orbiton121007@gmail.com>
2025-08-28 12:51:27 +0300

    function

--- filterting2.py
+++ filterting2.py
@@ -4,8 +4,12 @@ import filter_information
 from filter_information import get_information
 
 def write_to_data_file(food_service_dict): #writes the information onto the csv file
-    panda = pd.DataFrame(food_service_dict)
-    Data = str(get_information(panda))
+    Data = str(food_service_dict)
     with open("data.cvs","a") as f:
         f.write(Data)
+
+def filtering_kosher(food_service_dict, user_list):
+    if user_list[1]: # if the user filters to kosher:
+
+
 
