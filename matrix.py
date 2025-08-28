ba5c920c7f852b0cde2247a7d64f9b4e0f004113
Dana - Ezra <zinaezra@gmail.com>
2025-08-28 10:14:15 +0300

    get info

--- matrix.py
+++ matrix.py
@@ -20,7 +20,7 @@ def matrix_to_screen(matrix1,screen):
             for v in range(PIXEL_PER_BLOCK):
                     list1.append((x,y))
                     x += 1
-            dict_values[(i//36,j//36)] = list1
+            dict_values[(i//10,j//10)] = list1
 
     return print(dict_values)
 
