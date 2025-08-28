c3af157994eaee43e85feedb121024ffca095b97
Danielle036 <danielle.mofkadi@gmail.com>
2025-08-28 11:28:05 +0300

    Initial commit

--- maps.py
+++ maps.py
@@ -1,1 +1,16 @@
-print()
+import webbrowser
+import folium
+from IPython.core.display import display
+from geopy.geocoders import Nominatim
+
+loc = Nominatim(user_agent="Geopy Library")
+
+getLoc = loc.geocode(input("Enter location: "))
+print(getLoc.latitude, getLoc.longitude)
+
+location=[getLoc.latitude, getLoc.longitude]
+m = folium.Map(location, zoom_start=18)
+folium.Marker(location,tooltip='your house?').add_to(m)
+m.save('map.html')
+new = 2
+webbrowser.open('map.html', new=new)
